5e12b224f71368efd13cf21c89cd48e0e8ab0798
mmcfadye <mmcfadye@gmail.com>
2014-05-23 23:54:00 -0600

    after "Creating a UI Fragment"

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/app/CrimeFragment.java
@@ -0,0 +1,50 @@
+package com.bignerdranch.android.criminalintent.app;
+
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by mmcfa_000 on 5/23/2014.
+ */
+public class CrimeFragment extends android.support.v4.app.Fragment {
+    private Crime crime;
+    private EditText titleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        titleField = (EditText) view.findViewById(R.id.crime_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                crime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return view;
+    }
+}
